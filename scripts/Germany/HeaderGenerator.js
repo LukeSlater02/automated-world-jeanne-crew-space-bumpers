e21d8dd17ea05daa5e5928903c8420f66ef394b3
Houston-Smith <houstonbsmith@gmail.com>
2022-03-03 13:13:23 -0600

    Header HTML

---
+++ scripts/Germany/HeaderGenerator.js
@@ -0,0 +1,22 @@
+export const Header = (countryObj) => {
+ return `<section class="backdrop">
+      <section class="card">
+        <header>
+        <nav class="navbar">
+            <a class="country" href="index.html" title="Canada">Canada</a>
+            <a class="country" href="index.html" title="England">England</a>
+        </nav>
+            <a class= "logo" href="" title=""></a>
+        <nav class="navbar"> 
+            <a id="currentNav" href="index.html" title="Germany">Germany</a>
+            <a class="country" href="index.html" title="Japan">Japan</a>
+        </nav>
+        </header> 
+        <header class="background">
+          <h1>${countryObj.name}</h1>
+          <h3>${countryObj.info}</h3>
+          <h2>${countryObj.location}</h2>
+        </header>
+      </section>
+  </section>`
+}
