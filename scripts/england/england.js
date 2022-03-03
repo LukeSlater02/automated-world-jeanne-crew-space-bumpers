0c7f2e750ec1778109515df5bbad7b48d196cbfd
jandershock <jordan.andershock@gmail.com>
2022-03-03 14:21:46 -0600

    Moved css to correct locations

--- scripts/england/england.js
+++ scripts/england/england.js
@@ -1,3 +1,14 @@
 import * as footer from '../../scripts/shared/footer.js';
+import * as header from '../shared/HeaderGenerator.js';
 
+// Generate Header
+document.querySelector('header').innerHTML = header.Header({
+    name: "England",
+    info: "This is England!",
+    location: "Picture of Big Ben goes here . . ."
+})
+
+// Generate Main Section
+
+// Generate Footer
 footer.insertFooterHTML();
