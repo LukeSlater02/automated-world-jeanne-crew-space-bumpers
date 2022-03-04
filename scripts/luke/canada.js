7fcd40530d3560d54120a8aa361ef11d37e8a7ab
LukeSlater02 <lukeslater101@gmail.com>
2022-03-04 09:54:37 -0600

    .

--- scripts/luke/canada.js
+++ scripts/luke/canada.js
@@ -1,9 +1,18 @@
-import {cityObj} from "./canadaData.js"
-import {landmarkObj} from "./canadaData.js"
-import {citizenObj} from "./canadaData.js"
-import { generateLandmarkHTML } from "./canadaHTMLfunc.js"
-import {generateCityHTML} from "./canadaHTMLfunc.js"
-import { generateCitizenHTML } from "./canadaHTMLfunc.js"
+// OBJ INTO HTML
+import {cityObj, landmarkObj, citizenObj} from "./canadaData.js"
+import {generateLandmarkHTML, generateCitizenHTML, generateCityHTML} from "./canadaHTMLfunc.js"
+
+// HEADER & FOOTER
+import {Header} from "../shared/HeaderGenerator.js"
+import {insertFooterHTML} from "../shared/footer.js"
+
+insertFooterHTML("footer");
+
+document.querySelector("header").innerHTML = Header({
+    name: "Canada",
+    location:"picture subtitle here",
+    info:"A Mari Usque Ad Mare",
+})
 
 for (const landmark of landmarkObj){
     generateLandmarkHTML(landmark)
