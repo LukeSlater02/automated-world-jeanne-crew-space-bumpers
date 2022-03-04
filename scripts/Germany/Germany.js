41e0f54d6e17cd216da6e02dbc0524b4b49d19df
Houston-Smith <houstonbsmith@gmail.com>
2022-03-04 10:12:05 -0600

    Germany Card

--- scripts/Germany/Germany.js
+++ scripts/Germany/Germany.js
@@ -5,9 +5,11 @@ import * as germanData from './GermanyData.js';
 
 // Generate Header
 document.querySelector('header').innerHTML = header.Header({
-    name: "Germany",
-    info: "This is Germany!",
-    location: "Picture goes here . . ."
+    name: "GERMANY",
+    info: "Various Germanic tribes have inhabited the northern parts of modern Germany since classical antiquity. A region named Germania was documented before AD 100. In the 10th century, German territories formed a central part of the Holy Roman Empire. During the 16th century, northern German regions became the centre of the Protestant Reformation.Germany is a great power with a strong economy; it has the largest economy in Europe, the world's fourth-largest economy by nominal GDP, and the fifth-largest by PPP. As a global leader in several industrial, scientific and technological sectors, it is both the world's third-largest exporter and importer of goods. As a developed country, which ranks very high on the Human Development Index, it offers social security and a universal health care system, environmental protections, and a tuition-free university education. Germany is a member of the United Nations, NATO, the G7, the G20, and the OECD. It has the third-greatest number of UNESCO World Heritage Sites.",
+    location: "Brandenburg Gate",
+    pathToBackgroundImage: "../../images/germany/GermanyBackground1.png",
+    pathToRoot: "../../"
 })
 
 // Generate Main Section
