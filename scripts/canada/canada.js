4b73eb15553e9ea88c1776371a19c9f5c8077098
LukeSlater02 <lukeslater101@gmail.com>
2022-03-04 10:55:54 -0600

    m

--- scripts/canada/canada.js
+++ scripts/canada/canada.js
@@ -3,25 +3,30 @@ import {cityObj, landmarkObj, citizenObj} from "./canadaData.js"
 import {generateLandmarkHTML, generateCitizenHTML, generateCityHTML} from "./canadaHTMLfunc.js"
 
 // HEADER & FOOTER
-import {Header} from "../shared/HeaderGenerator.js"
-import {insertFooterHTML} from "../shared/footer.js"
+import * as footer from '../../scripts/shared/footer.js';
+import * as header from '../shared/HeaderGenerator.js';
 
-insertFooterHTML("footer");
-
-document.querySelector("header").innerHTML = Header({
+document.querySelector('header').innerHTML = header.Header({
     name: "Canada",
-    location:"picture subtitle here",
-    info:"A Mari Usque Ad Mare",
+    info: "A Mari Usque Ad Mare",
+    location: "Picture of Lake Louise goes here . . .",
+    pathToBackgroundImage: "../../images/canada/lakelouise.png",
+    pathToRoot: "../../"
 })
 
+// landmark loop
 for (const landmark of landmarkObj){
     generateLandmarkHTML(landmark)
 }
 
+// city loop
 for (const city of cityObj){
     generateCityHTML(city)
 }
 
+// citizen loop`
 for (const citizen of citizenObj){
     generateCitizenHTML(citizen)
 }
+
+footer.insertFooterHTML();
