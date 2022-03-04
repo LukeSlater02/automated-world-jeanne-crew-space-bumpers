80b7e62e37fd7714983b2f3eea55890f0f2a26d1
ToryT23 <torjtheu@gmail.com>
2022-03-04 13:51:24 -0600

    updates to japan html

--- scripts/japan/Japan.js
+++ scripts/japan/Japan.js
@@ -5,10 +5,10 @@ import {insertFooterHTML} from "../shared/footer.js"
 
 document.querySelector("header").innerHTML = Header({
     name: "Japan",
-    location: "Randon Picture Sub Title",
-    info: "Japan is cool",
+    location: "Nihon e Youkoso",
+    info: "Japan is an archipelago of some 6,852 islands located in a volcanic zone on the Pacific Ring of Fire. The Pacific Ring of Fire accounts for more than 75 percent of the world’s active volcanoes and 90 percent of the world’s earthquakes. It is a constitutional monarchy, with Tokyo as its capital. More than 99% of the population speaks Japanese. The economy of Japan is the third largest in the world after the United States and the People's Republic of China.",
     pathToRoot: "../../",
-    pathToBackgroundImage:"../../images/japan/japanStreets.png"
+    pathToBackgroundImage:"../../images/japan/JapanHeader.png"
 
 })
 
